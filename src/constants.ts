2b42817e2dea1280623b45af4d8c62d17331e26c
Jaeden <ch.schwartze@gmail.com>
2024-06-07 11:36:59 +0200

    chore: remove subdomain

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const WPLATEST_ACTIONS = ['create-new-version'] as const
-export const WPLATEST_API_BASE = 'https://www.wplatest.co/api/v1'
+export const WPLATEST_API_BASE = 'https://wplatest.co/api/v1'
