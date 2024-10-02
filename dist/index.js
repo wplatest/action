b907bd9e3c00322d48b0d2f7623c98800a73d296
Jaeden <ch.schwartze@gmail.com>
2024-10-02 09:50:27 +0200

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -24753,7 +24753,7 @@ const utils_1 = __nccwpck_require__(442);
  */
 async function run() {
     try {
-        const { ARTIFACT_URL, GITHUB_TOKEN, WPLATEST_SECRET, WPLATEST_ACTION, WPLATEST_PLUGIN_ID, WPLATEST_SLUG } = (0, utils_1.getWorkflowInput)();
+        const { ARTIFACT_URL, GITHUB_TOKEN, WPLATEST_SECRET, WPLATEST_ACTION, WPLATEST_PLUGIN_ID } = (0, utils_1.getWorkflowInput)();
         if (!constants_1.WPLATEST_ACTIONS.includes(WPLATEST_ACTION)) {
             core.setFailed(`Invalid action: ${WPLATEST_ACTION}. Must be one of: ${constants_1.WPLATEST_ACTIONS.join(', ')}`);
             return;
@@ -24762,7 +24762,6 @@ async function run() {
             core.info(`Creating new plugin version: ${WPLATEST_PLUGIN_ID}`);
             core.info(`Using artifact URL: ${ARTIFACT_URL}`);
             const config = {
-                slug: WPLATEST_SLUG,
                 zip_url: ARTIFACT_URL,
                 plugin_id: WPLATEST_PLUGIN_ID
             };
@@ -24779,6 +24778,8 @@ async function run() {
                 }
                 const data = (await response.json());
                 core.info(`New version created: ${data.id} - ${data.version}`);
+                core.setOutput('id', data.id);
+                core.setOutput('version', data.version);
             }
             catch (err) {
                 const error = err;
@@ -24819,15 +24820,13 @@ const getWorkflowInput = () => {
     const WPLATEST_SECRET = (0, core_1.getInput)('wplatest-secret', { required: true });
     const WPLATEST_ACTION = (0, core_1.getInput)('wplatest-action');
     const WPLATEST_PLUGIN_ID = (0, core_1.getInput)('wplatest-plugin-id');
-    const WPLATEST_SLUG = (0, core_1.getInput)('wplatest-slug', { required: true });
     const ARTIFACT_URL = (0, core_1.getInput)('wplatest-artifact-zip-url');
     return {
         ARTIFACT_URL,
         GITHUB_TOKEN,
         WPLATEST_SECRET,
         WPLATEST_ACTION,
-        WPLATEST_PLUGIN_ID,
-        WPLATEST_SLUG
+        WPLATEST_PLUGIN_ID
     };
 };
 exports.getWorkflowInput = getWorkflowInput;
